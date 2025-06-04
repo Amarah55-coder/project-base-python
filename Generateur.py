02413b21dd0b8998567264845ff2b2dbf22ae41d
Amarah55-coder <MaheryRAZAFIMANANTSOA55@gmail.com>
2025-06-04 21:35:27 +0300

    Importation des modules préalable pour le projet

---
+++ Generateur.py
@@ -0,0 +1,13 @@
+import random
+import string 
+from string import *
+
+minuscule_letter = ascii_lowercase
+print(minuscule_letter)
+majuscule_letter = ascii_uppercase
+print(majuscule_letter)
+
+char = "%"
+
+if char in string.punctuation:
+    print(f"{char}")
