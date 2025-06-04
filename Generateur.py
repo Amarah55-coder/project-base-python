1c437dba81c7d0d4782b3853bf85f5090e47362d
Amarah55-coder <MaheryRAZAFIMANANTSOA55@gmail.com>
2025-06-04 22:12:41 +0300

    ajout des certains concepts

--- Generateur.py
+++ Generateur.py
@@ -1,7 +1,24 @@
 import random
-import string 
-from string import *
+import string
 
-format_mot_de_passe = ascii_letters + digits + punctuation
+try:
+    longueur_mot_de_passe = int(input("Entrer la longueur de votre mot de passe: "))
+except ValueError:
+    print("Veuillez entrer un nombre entier.")
+    exit()
 
+if longueur_mot_de_passe < 8:
+    mot_de_passe = "Mot de passe trop court"
+else:
+    format_mot_de_passe = string.ascii_letters + string.digits + string.punctuation
+    mot_de_passe = [
+        random.choice(string.ascii_lowercase),
+        random.choice(string.ascii_uppercase),
+        random.choice(string.digits),
+        random.choice(string.punctuation)
+    ]
+    mot_de_passe += [random.choice(format_mot_de_passe) for _ in range(longueur_mot_de_passe - 4)]
+    random.shuffle(mot_de_passe)
+    mot_de_passe = ''.join(mot_de_passe)
 
+print(f"Mot de passe généré: {mot_de_passe}")
